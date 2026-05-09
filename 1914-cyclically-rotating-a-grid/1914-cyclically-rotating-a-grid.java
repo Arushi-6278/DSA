d52fea4a8a4c3f11a328446c329ee61d29c56c26
Arushi-6278 <98529905+Arushi-6278@users.noreply.github.com>
2026-05-09 11:39:50 +0530

    Time: 6 ms (44.78%), Space: 46.8 MB (98.51%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] rotateGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int numLayers = Math.min(m, n) / 2;
+
+        for (int layer = 0; layer < numLayers; layer++) {
+       
+            List<Integer> elements = new ArrayList<>();
+            
+           
+            for (int j = layer; j < n - layer - 1; j++) elements.add(grid[layer][j]);
+           
+            for (int i = layer; i < m - layer - 1; i++) elements.add(grid[i][n - layer - 1]);
+        
+            for (int j = n - layer - 1; j > layer; j--) elements.add(grid[m - layer - 1][j]);
+        
+            for (int i = m - layer - 1; i > layer; i--) elements.add(grid[i][layer]);
+
+           
+            int size = elements.size();
+            int netRotation = k % size;
+
+           
+            int index = 0;
+            
+            // Top row
+            for (int j = layer; j < n - layer - 1; j++) 
+                grid[layer][j] = elements.get((index++ + netRotation) % size);
+        
+            for (int i = layer; i < m - layer - 1; i++) 
+                grid[i][n - layer - 1] = elements.get((index++ + netRotation) % size);
+          
+            for (int j = n - layer - 1; j > layer; j--) 
+                grid[m - layer - 1][j] = elements.get((index++ + netRotation) % size);
+          
+            for (int i = m - layer - 1; i > layer; i--) 
+                grid[i][layer] = elements.get((index++ + netRotation) % size);
+        }
+
+        return grid;
+    }
+}
