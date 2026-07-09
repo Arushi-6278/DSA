98c61d729e20043f3484e3f275132e4ef4c40e31
Arushi-6278 <98529905+Arushi-6278@users.noreply.github.com>
2026-07-09 12:34:50 +0530

    Time: 3 ms (97.03%), Space: 162.9 MB (77.23%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] component = new int[n];
+        int currentComponent = 0;
+        component[0] = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                currentComponent++;
+            }
+            component[i] = currentComponent;
+        }
+        
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = component[queries[i][0]] == component[queries[i][1]];
+        }
+        
+        return ans;
+    }
+}
