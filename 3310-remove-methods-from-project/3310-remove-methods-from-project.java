dba25b189966ef57b4c80240a9b612d8d2ba2362
Arushi-6278 <98529905+Arushi-6278@users.noreply.github.com>
2026-08-05 14:40:13 +0530

    Time: 59 ms (82.69%), Space: 274.7 MB (93.27%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] inv : invocations) {
+            graph.get(inv[0]).add(inv[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(k);
+        suspicious[k] = true;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int neighbor : graph.get(curr)) {
+                if (!suspicious[neighbor]) {
+                    suspicious[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        for (int[] inv : invocations) {
+            int u = inv[0];
+            int v = inv[1];
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> result = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    result.add(i);
+                }
+                return result;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
